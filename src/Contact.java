ea07e2ceba6b22cd7315bfe51e75a0c204e37a74
kanishka1312 <kanishkaagarwa1312@gmail..com>
2024-11-30 14:15:15 +0530

    Added:Displaying Contact

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Contact {
+    String firstName;
+    String lastName;
+    String city;
+    String state;
+    String email;
+    int phone;
+    int zip;
+
+    public Contact(String firstName,String lastName,String city,String state,String email,int phone,int zip){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.city=city;
+        this.state=state;
+        this.email=email;
+        this.phone=phone;
+        this.zip=zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", zip=" + zip +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AddressBook a1=new AddressBook();
+        int i=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to Address Book");
+        do{
+            System.out.println("Enter 1 to create contact \n Enter 2 to display all contacts");
+            int input=sc.nextInt();
+            switch (input){
+                case 1-> a1.createContact();
+                case 2-> a1.display();
+                default -> System.out.println("wrong input");
+            }
+            System.out.println("Enter 0 to exit");
+            i=sc.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        }while(i!=0);
+        System.out.println("Thanks for visit!");
+        sc.close();
+
     }
+
 }
